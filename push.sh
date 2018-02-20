ec4e1c9674e1f3d8b22c80ca17f6c783aeb053eb
CompulsiveCoder <compulsivecoder@gmail.com>
2018-02-20 22:53:00 +0000

    Added tag and push scripts

---
+++ push.sh
@@ -0,0 +1,4 @@
+IMAGE_NAME=jenkins-arm
+DOCKER_ID=compulsivecoder
+
+docker push $DOCKER_ID/$IMAGE_NAME
---
+++ tag.sh
@@ -0,0 +1,4 @@
+IMAGE_NAME=jenkins-arm
+DOCKER_ID=compulsivecoder
+
+docker tag $IMAGE_NAME $DOCKER_ID/$IMAGE_NAME
